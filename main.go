e0c61cf13eeac79131ba7c2dade1e8dae4822268
lowbridgee <lowbridge.highbridge@gmail.com>
2018-12-23 16:37:03 +0900

    add idol search

--- main.go
+++ main.go
@@ -20,8 +20,7 @@ func main() {
 			Name: "find_unit",
 			Usage: "Find unit name by idol name.",
 			Action: func(c *cli.Context) error {
-				name := os.Args[2]
-				res := findUnitByMemberName(name)
+				res := findUnitByMemberName(os.Args[2])
 				m := res.Results.Bindings
 				for _, v := range m {
 					println(v["ユニット名"].Value)
@@ -31,8 +30,17 @@ func main() {
 		},
 		{
 			Name: "grep",
-			Usage: "find idol",
+			Usage: "Find idol by idol name.",
 			Action: func(c *cli.Context) error {
+				res := findIdolByName(os.Args[2])
+				m := res.Results.Bindings
+
+				for _, v := range m {
+					for k := range v {
+						println(k + ": " + v[k].Value)
+					}
+				}
+
 				return nil
 			},
 		},
@@ -43,6 +51,7 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
 func findUnitByMemberName(name string) *sparql.Results {
 	repo, err := sparql.NewRepo("https://sparql.crssnky.xyz/spql/imas/query")
 	if err != nil {
@@ -71,6 +80,46 @@ func findUnitByMemberName(name string) *sparql.Results {
 	bank := sparql.LoadBank(f)
 	
 	sql, err := bank.Prepare("find-unit-by-member-idol", struct{ Name string }{name})
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	res, err := repo.Query(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res
+}
+
+func findIdolByName(name string) *sparql.Results {
+	repo, err := sparql.NewRepo("https://sparql.crssnky.xyz/spql/imas/query")
+	if err != nil {
+		log.Fatal(err)
+	}
+	const query = `
+# tag: find-idol-by-name
+	PREFIX schema: <http://schema.org/>
+	PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+	PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>
+	PREFIX imasrdf: <https://sparql.crssnky.xyz/imasrdf/RDFs/detail/>
+	PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+	PREFIX math: <http://www.w3.org/2005/xpath-functions/math#>
+	PREFIX xsd: <https://www.w3.org/TR/xmlschema11-2/#>
+	PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+	SELECT ?名前 ?身長 ?体重 ?年齢
+	WHERE {
+		?s rdf:type imas:Idol;
+			schema:name ?名前;
+			schema:height ?身長;
+			schema:weight ?体重;
+			foaf:age ?年齢;
+			
+		filter contains (?名前, "{{.Name}}").
+	}`
+	f := bytes.NewBufferString(query)
+	bank := sparql.LoadBank(f)
+	
+	sql, err := bank.Prepare("find-idol-by-name", struct{ Name string }{name})
 	if err != nil {
 		log.Fatal(err)
 	}
